cb183391c94f58443339910a152329e6299675fd
TARIK ONAL <tarikonal@gmail.com>
2020-01-31 01:11:37 +0300

    Add project files.

---
+++ AudioPlayer/AudioPlayerPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using AudioPlayer.Services;
+using AudioPlayer.ViewModels;
+using System;
+using System.IO;
+using System.Reflection;
+using Xamarin.Forms;
+
+namespace AudioPlayer
+{
+	public partial class AudioPlayerPage : ContentPage
+	{
+		public AudioPlayerPage()
+		{
+			InitializeComponent();
+			BindingContext = new AudioPlayerViewModel(DependencyService.Get<IAudioPlayerService>());
+		}
+	
+	}
+}
---
+++ AudioPlayer/ViewModels/AudioPlayerViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using AudioPlayer.Services;
+using Xamarin.Forms;
+
+namespace AudioPlayer.ViewModels
+{
+	public class AudioPlayerViewModel: INotifyPropertyChanged
+	{
+		private IAudioPlayerService _audioPlayer;
+		private bool _isStopped;
+		private int _indexOfButton;
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public AudioPlayerViewModel(IAudioPlayerService audioPlayer)
+		{
+			_audioPlayer = audioPlayer;
+			_audioPlayer.OnFinishedPlaying = () => {
+				_isStopped = true;
+				CommandText = "Play";
+				_indexOfButton = 0;
+			};
+			CommandText = "Play";
+			_isStopped = true;
+			_indexOfButton = 0;
+		}
+
+		private string _commandText;
+		public string CommandText
+		{
+			get { return _commandText;}
+			set
+			{
+				_commandText = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CommandText"));
+			}
+		}
+
+		private ICommand _playPauseCommand;
+		public ICommand PlayPauseCommand
+		{
+			get
+			{
+				return _playPauseCommand ?? (_playPauseCommand = new Command(
+					(obj) => 
+				{
+					if (CommandText == "Play")
+					{
+						if (_isStopped)
+						{
+							_isStopped = false;
+							_audioPlayer.Play("Galway.mp3");
+						}
+						else
+						{
+
+							_audioPlayer.Play();
+						}
+						CommandText = "Pause";
+					}
+					else
+					{
+						_audioPlayer.Pause();
+						CommandText = "Play";
+					}
+				}));
+			}
+		}
+	}
+}
